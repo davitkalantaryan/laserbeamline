31bebe27b9d4b51e2899ead844fefa0c2f43a429
Davit Kalantaryan <davit.kalantar@desy.de>
2019-02-15 13:17:24 +0100

    modified main of API doc

--- doxygen/doxygen_gen.cpp
+++ doxygen/doxygen_gen.cpp
@@ -3,15 +3,11 @@
  *  
  *  @section intro Introduction
  * 
- *  This is the DOOCS server library, being the central part of every
- *  DOOCS server. This library is available as static or shared library
- *  and called libEqServer.a or libEqServer.so.\n
- *  The two most important classes are EqFct as location class and D_fct for all the property types\n
- *  Every DOOCS server needs to be linked at least against this library
- *  and the DOOCS client library libDOOCSapi.
- *  The file eq_server.cc contains the main() function for a server.
+ *  Here the API for PITZ laserbeamline control is described.\n
+ *  As a control system DOOCS is used (http://tesla.desy.de/doocs/doocs.html). \n
+ * 
  * 
  *  @author   Davit, Kalantaryan
  *
- *  @date     29.03.2006
+ *  @date     2019 Feb 15
 **/
